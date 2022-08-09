bb31ef20e71005ab71a94e3dd3818b110d443d05
ConquerorKapson <69422202+ConquerorKapson@users.noreply.github.com>
2022-08-09 23:07:36 +0530

    improved code

--- Recursion/ClimbingStairs_LeetCode70/ClimbingStairs.cpp
+++ Recursion/ClimbingStairs_LeetCode70/ClimbingStairs.cpp
@@ -13,12 +13,34 @@ int climbStairs(int n){
     return climbStairs(n-1)+climbStairs(n-2);    
 }
 
+ int climbStairs2(int n) {
+        int prev = 1;
+        int present = 1;
+        int next = 0;
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        
+        for(int stair = 2; stair <= n ; stair++){
+            next = prev+present;
+            prev = present;
+            present=next;
+        }
+        return next;
+    }
+
 int main(){
 
     int n;
     cout<<"Number of stairs? ";
     cin >> n;
 
+    int number_of_ways2 = climbStairs2(n);
+    cout << "number of ways to climb stairs effective: " << number_of_ways2;
+    
     int number_of_ways = climbStairs(n);
     cout << "number of ways to climb stairs: " << number_of_ways;
 
