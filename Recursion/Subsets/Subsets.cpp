b71fd6df96331d25e9fb4b310ff4fbf56237dd42
ConquerorKapson <69422202+ConquerorKapson@users.noreply.github.com>
2022-08-13 19:45:18 +0530

    made some shitty subsets

---
+++ Recursion/Subsets/Subsets.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void subsetHelper(vector<vector<int>>& result, vector<int>& nums, int index, int lastIndex, vector<int>subset){
+    if(index > lastIndex){
+        result.push_back(subset);
+        return;
+    }
+    // exclude
+    subsetHelper(result, nums, index+1, lastIndex, subset);
+    
+    // include
+    subset.push_back(nums[index]);
+    subsetHelper(result, nums, index+1, lastIndex, subset);
+}   
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    int index = 0;
+    int lastIndex = nums.size()-1;
+    vector<int>subset;
+    vector<vector<int>> result;
+    subsetHelper(result, nums, index, lastIndex, subset);
+    return result;
+}
+
+int main(){
+    vector<int>nums{2, 3, 5};
+    vector<vector<int>>ans = subsets(nums);
+
+    for(vector<int> i : ans){
+        for(int x: i){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
+
