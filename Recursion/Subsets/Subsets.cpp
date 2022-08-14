70abb0c5041c08303e58220f151b9cce6f34eb10
Swaranshulendraditya Kapoor <t-swaranshulendraditya.kapoor@ocltp.com>
2022-08-15 00:23:08 +0530

    commit

--- Recursion/Subsets/Subsets.cpp
+++ Recursion/Subsets/Subsets.cpp
@@ -29,7 +29,6 @@ int main(){
     vector<int>nums{2, 8, 6, 10, 20};
     vector<vector<int>>ans = subsets(nums);
 
-    //dummy commit
     for(vector<int> i : ans){
         cout<<"{";
         for(int x: i){
