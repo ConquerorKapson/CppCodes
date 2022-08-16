308cc997f3224ada243df99730a0eaf251b2b91b
ConquerorKapson <69422202+ConquerorKapson@users.noreply.github.com>
2022-08-17 00:49:22 +0530

    DP initiated

---
+++ DynamicProgramming/MinCostClimbingStairs_LeetCode746/MinCostClimbingStairs.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+
+int minCostClimbingStairs(vector<int>cost){
+    
+}
+
+
+int main(){
+    vector<int> costs{1, 20, 15, 14, 2, 9, 63, 22};
+    int minCost = int minCostClimbingStairs(costs);
+    cout<<"Min cost to reach is : "<<cost<<endl;
+    return 0;
+}
---
+++ DynamicProgramming/MinNumberOfCoins/MinCoins.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int calculate(int n){
+    int* dp = new int[n+1];
+    dp[0] = ;
+    dp[1] = ;
+
+    for(int i = 2 ; i <=n ; i++){
+
+    }
+
+    int ans = dp[n];
+    delete [] dp
+    return ans;
+}
+
+int main(){
+    int n;
+    cout<<"Enter your number :  ";
+    cin>>n;
+
+    cout<<calculate(n);
+
+    return 0;
+}
