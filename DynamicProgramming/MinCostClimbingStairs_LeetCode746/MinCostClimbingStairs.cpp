2fce54268ac4f48055d4995abc87fc506aa94621
ConquerorKapson <69422202+ConquerorKapson@users.noreply.github.com>
2022-08-20 16:31:20 +0530

    Min Cost climbing stairs solved suing DP

--- DynamicProgramming/MinCostClimbingStairs_LeetCode746/MinCostClimbingStairs.cpp
+++ DynamicProgramming/MinCostClimbingStairs_LeetCode746/MinCostClimbingStairs.cpp
@@ -1,16 +1,32 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 
 
-int minCostClimbingStairs(vector<int>cost){
-    
+int minCostClimbingStairs(vector<int>& cost){
+    int n = cost.size();
+    vector<int>dp(n, -1);
+
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+
+    for(int i = 2 ; i < n ; i++){
+        dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        cout<<dp[i]<<" ";
+    }
+    cout<<endl;
+    int ans = min(dp[n-1], dp[n -2]);
+    return ans;
 }
 
 
 int main(){
-    vector<int> costs{1, 20, 15, 14, 2, 9, 63, 22};
-    int minCost = int minCostClimbingStairs(costs);
-    cout<<"Min cost to reach is : "<<cost<<endl;
+    vector<int>costs{1, 20, 15, 14, 2, 9, 63, 22};
+    int minCost = minCostClimbingStairs(costs);
+    cout<<"Min cost to reach is : "<<minCost<<endl;
     return 0;
 }
