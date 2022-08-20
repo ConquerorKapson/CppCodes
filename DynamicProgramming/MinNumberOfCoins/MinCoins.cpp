00bb766fa0cc3404cdff13fdcc727938b612a1ab
ConquerorKapson <69422202+ConquerorKapson@users.noreply.github.com>
2022-08-20 18:42:12 +0530

    ek aur code hag diya <3

--- DynamicProgramming/MinNumberOfCoins/MinCoins.cpp
+++ DynamicProgramming/MinNumberOfCoins/MinCoins.cpp
@@ -1,26 +1,47 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int calculate(int n){
-    int* dp = new int[n+1];
-    dp[0] = ;
-    dp[1] = ;
+int calculate(vector<int> coins, int amount){
+    vector<int>dp(amount+1, INT_MAX);
+    dp[0] = 0;
 
-    for(int i = 2 ; i <=n ; i++){
-
+    for(int i = 1 ; i <=amount ; i++){
+        for(int j = 0 ; j < coins.size() ; j++){
+            if((i-coins[j]) >= 0 && dp[i- coins[j]] != INT_MAX){
+                dp[i] = min(dp[i], 1 + dp[i-coins[j]]);
+            }
+        }
     }
 
-    int ans = dp[n];
-    delete [] dp
+    int ans = INT_MAX;
+    if(dp[amount] == INT_MAX)
+        ans = -1;
+    else
+        ans = dp[amount];
+    
     return ans;
 }
 
 int main(){
     int n;
-    cout<<"Enter your number :  ";
+    cout<<"Enter your number of coins :  ";
     cin>>n;
 
-    cout<<calculate(n);
+    cout<<"Enter your coins denomination : ";
+    vector<int> coins;
+
+    for(int i = 0 ; i < n ; i++){
+        int coin;
+        cin>>coin;
+
+        coins.push_back(coin);
+    }
+
+    int amount;
+    cout<<"Enter the amount : ";
+    cin>>amount;
+    cout<<calculate(coins, amount);
 
     return 0;
 }
